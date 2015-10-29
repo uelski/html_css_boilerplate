a363499e9eb11ff277c9286ddd5b90494208e8f2
Sam Vredenburgh <sam.vburgh@gmail.com>
2015-10-29 18:50:21 -0500

    madlib practice

--- scripts/app.js
+++ scripts/app.js
@@ -5,12 +5,19 @@ window.onload = function() {
   //alert('hey there buddy');
   statusReport("hey there buddy");
 
-  var username = prompt("What is your name?");
-  var mission = prompt("What is your mission?");
+  var furniture = prompt("What is your favorite piece of furniture");
+  var passion = prompt("What was your passion as child?");
+  var middleName = prompt("What is your middle name?");
+  var hometown = prompt("What is the name of your hometown?");
+
+
 
   var contentContainer = document.getElementById('content');
 
-  contentContainer.innerHTML = username + ' is on a mission to ' + mission;
+  contentContainer.innerHTML = "Mr. " + furniture + " is going to the " + passion + " factory to see " + middleName + " " + hometown + ".";
+  var changingImg = document.getElementById("y").src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSYoXyq4YXD8ProG4ch0nDjzC2T9_xiSY68E5C0PL-8Yij8dvxkHw"
+
+
 }
 
 function statusReport(message) {
