02782d1e4c5ab892be827c8f26a551ecf10e064e
Sam Vredenburgh <sam.vburgh@gmail.com>
2015-10-29 16:19:54 -0500

    mission update

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,13 @@
 window.onload = function() {
   //alert('hey there buddy');
   statusReport("hey there buddy");
+
+  var username = prompt("What is your name?");
+  var mission = prompt("What is your mission?");
+
+  var contentContainer = document.getElementById('content');
+
+  contentContainer.innerHTML = username + ' is on a mission to ' + mission;
 }
 
 function statusReport(message) {
